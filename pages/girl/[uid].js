c0df3623d12c6c763713be089c94d8578b3978e0
adam wright <adam.wright90@yahoo.co.uk>
2021-03-02 19:21:11 +0000

    no message

--- pages/girl/[uid].js
+++ pages/girl/[uid].js
@@ -1,23 +1,25 @@
 import { client } from "../../prismic-configuration";
 import { RichText } from "prismic-reactjs";
 import Prismic from "prismic-javascript";
-
+import Layout from "../../components/Layout"
 import styles from '../../styles/components/girl.module.scss'
 
 
 export default function Post({ data }) {
   console.log(data);
   return (
-    <div className="container">
-      <div className="row">
-        <div className={styles.imageContainer}>
-          <img src={data.main_image.url} alt={data.main_image.alt}/>
-        </div>
-        <div className={styles.detailsContainer}>
-          <h1 className={styles.name}>{data.name[0].text}</h1>
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className={styles.imageContainer}>
+            <img src={data.main_image.url} alt={data.main_image.alt}/>
+          </div>
+          <div className={styles.detailsContainer}>
+            <h1 className={styles.name}>{data.name[0].text}</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
--- pages/good.js
+++ pages/good.js
@@ -2,30 +2,29 @@ import { client } from "../prismic-configuration";
 import { RichText } from "prismic-reactjs";
 import Prismic from "prismic-javascript";
 import Link from "next/link"
-import Navigation from "../components/Navigation"
+import Layout from '../components/Layout'
 import styles from '../styles/components/list.module.scss'
 
 function good({ data }) {
     return (
-        <>
-        <Navigation></Navigation>
-        <div className="container">
-            <div className="row">
-                 {
-                     data.results.map((item, index) => 
-                        <div className={styles.item} key={index}>
-                            <Link href={`/girl/${encodeURIComponent(item.uid)}`}>
-                                <a  className={styles.link}>
-                                    <img src={item.data.main_image.url} alt={item.data.main_image.alt} className={styles.image}/>
-                                    <span className={styles.title}>{item.data.name[0].text}</span>
-                                </a>
-                            </Link>
-                        </div>
-                     )
-                 }
+        <Layout>
+            <div className="container">
+                <div className="row">
+                    {
+                        data.results.map((item, index) => 
+                            <div className={styles.item} key={index}>
+                                <Link href={`/girl/${encodeURIComponent(item.uid)}`}>
+                                    <a  className={styles.link}>
+                                        <img src={item.data.main_image.url} alt={item.data.main_image.alt} className={styles.image}/>
+                                        <span className={styles.title}>{item.data.name[0].text}</span>
+                                    </a>
+                                </Link>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
-        </div>
-    </>
+        </Layout>
     )
 }
 
